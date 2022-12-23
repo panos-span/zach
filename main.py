138a14d4a9a7d2197c250816906f62aba5129d94
Panagiotis Spanakis <t8200158@aueb.gr>
2022-12-23 14:30:47 +0200

    Bin Packing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import math
 import sol_checker
 import pprint
 
+
 class Node:
     def __init__(self, idd, xx, yy, dem=0, st=0):
         self.x = xx
@@ -14,7 +15,7 @@ class Node:
 
 class Model:
 
-# instance variables
+    # instance variables
     def __init__(self):
         all_nodes, vehicles, capacity = sol_checker.load_model('Instance.txt')
         self.allNodes = all_nodes
@@ -33,6 +34,7 @@ class Model:
                 dist = math.sqrt(math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2))
                 self.matrix[i][j] = dist
 
+
 def distance(from_node, to_node):
     dx = from_node.x - to_node.x
     dy = from_node.y - to_node.y
@@ -40,15 +42,48 @@ def distance(from_node, to_node):
     return dist
 
 
-#all_nodes, vehicles, capacity = sol_checker.load_model('Instance.txt')
-#for i in range(len(all_nodes)):
+class Route:
+    def __init__(self, dp, cap):
+        self.sequenceOfNodes = []
+        self.sequenceOfNodes.append(dp)
+        self.sequenceOfNodes.append(dp)
+        self.cost = 0
+        self.capacity = cap
+        self.load = 0
+
+
+# all_nodes, vehicles, capacity = sol_checker.load_model('Instance.txt')
+# for i in range(len(all_nodes)):
 #    print(all_nodes[i].ID,all_nodes[i].x, all_nodes[i].y, all_nodes[i].demand, all_nodes[i].serv_time)
 
 
-m = Model()
-m.BuildModel()
-pprint.pprint(m.matrix)
-for i in range(len(m.matrix)):
-    for j in range(len(m.matrix)):
-        if m.matrix[i][j] != m.matrix[j][i]:
-            print('ERROR')
+# m = Model()
+# m.BuildModel()
+# pprint.pprint(m.matrix)
+# for i in range(len(m.matrix)):
+#    for j in range(len(m.matrix)):
+#        if m.matrix[i][j] != m.matrix[j][i]:
+#            print('ERROR')
+
+
+def bin_packing():
+    m = Model()
+    m.BuildModel()
+    cap = [m.capacity for i in range(m.vehicles)]
+    bins = [[m.allNodes[0].ID] for i in range(m.vehicles)]
+    ids = [0 for i in range(0, len(m.allNodes))]
+    f = False
+    for node in m.allNodes:
+        if not f:
+            f = True
+            continue
+        for i in range(len(cap)):
+            if cap[i] >= node.demand:
+                bins[i].append(node.ID)
+                cap[i] -= node.demand
+                break
+    return bins, cap
+
+bins, cap = bin_packing()
+pprint.pprint(bins)
+pprint.pprint(cap)
